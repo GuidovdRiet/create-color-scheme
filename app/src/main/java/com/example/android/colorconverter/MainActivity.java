24416d0f7da76809ddc2e494a02cf940cba09718
Guido van de Riet <guidovdriet@gmail.com>
2018-04-10 15:34:31 +0200

    Add rearrange code splash screen

--- app/src/main/java/com/example/android/colorconverter/MainActivity.java
+++ app/src/main/java/com/example/android/colorconverter/MainActivity.java
@@ -1,30 +1,76 @@
 package com.example.android.colorconverter;
 
+import android.content.Context;
 import android.content.Intent;
-import android.os.Handler;
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
-    ImageView mLogoImageView;
-    final static int SPLASH_TIME_OUT = 2000;
+    private final static String HEX_VALUE = "hex";
+    private final static String RGB_VALUE = "rgb";
+    private final static String CMYK_VALUE = "cmyk";
+
+    private Button mHexButtonTextView;
+    private Button mRgbButtonTextView;
+    private Button mCmykButtonTextView;
+
+    private ImageView mBrandIconImageView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_home);
 
-        mLogoImageView = findViewById(R.id.iv_brand);
-        mLogoImageView.setImageResource(R.drawable.cd_brand);
+        mHexButtonTextView = findViewById(R.id.btn_hex_color);
+        mRgbButtonTextView = findViewById(R.id.btn_rgb_color);
+        mCmykButtonTextView = findViewById(R.id.btn_cmyk_color);
+        mBrandIconImageView = findViewById(R.id.iv_icon_brand);
 
-        new Handler().postDelayed(new Runnable() {
+        mBrandIconImageView.setImageResource(R.drawable.cd_brand_color_type_picker_icon);
+
+        mHexButtonTextView.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void run() {
-                Intent homeIntent = new Intent(MainActivity.this, HomeActivity.class);
-                startActivity(homeIntent);
+            public void onClick(View v) {
+                setIntent(
+                        MainActivity.this,
+                        ColorConvertActivity.class,
+                        Intent.EXTRA_TEXT,
+                        HEX_VALUE);
             }
-        }, SPLASH_TIME_OUT);
+        });
+
+        mRgbButtonTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setIntent(
+                        MainActivity.this,
+                        ColorConvertActivity.class,
+                        Intent.EXTRA_TEXT, RGB_VALUE
+                );
+            }
+        });
+
+        mCmykButtonTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setIntent(
+                        MainActivity.this,
+                        ColorConvertActivity.class,
+                        Intent.EXTRA_TEXT, CMYK_VALUE
+                );
+            }
+        });
+    }
+
+    private void setIntent(Context context, Class intentClass, String intentName, String intentData) {
+        Intent startColorConverter = new Intent(context, intentClass);
+        startColorConverter.putExtra(intentName, intentData);
+        startActivity(startColorConverter);
     }
 }
---
+++ app/src/main/java/com/example/android/colorconverter/SplashActivity.java
@@ -0,0 +1,36 @@
+package com.example.android.colorconverter;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class SplashActivity extends AppCompatActivity {
+
+    ImageView mLogoImageView;
+    final static int SPLASH_TIME_OUT = 2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mLogoImageView = findViewById(R.id.iv_brand);
+        mLogoImageView.setImageResource(R.drawable.cd_brand);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent homeIntent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(homeIntent);
+            }
+        }, SPLASH_TIME_OUT);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
+    }
+}
